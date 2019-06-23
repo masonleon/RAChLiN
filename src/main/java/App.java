20091a676b7b79f4dd5f524c0abd7af22c4d9dea
Mason <leon.m@husky.neu.edu>
2019-06-22 22:07:46 -0400

    adding files

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import dk.tbsalling.aismessages.demo.SimpleDemoApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        try {
+            SimpleDemoApp.main(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
