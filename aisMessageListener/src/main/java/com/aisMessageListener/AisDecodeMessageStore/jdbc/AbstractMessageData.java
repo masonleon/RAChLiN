70a2fd9292bdb496c26d0a5a03d8de1b2151bbed
Anand Kumar <kumara0398@gmail.com>
2019-07-24 18:09:00 -0400

    start refactoring by creating interface + abstract class

--- aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/AbstractMessageData.java
+++ aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/AbstractMessageData.java
@@ -1,17 +1,108 @@
 package com.aisMessageListener.AisDecodeMessageStore.jdbc;
 
+import dk.tbsalling.aismessages.ais.exceptions.UnsupportedMessageType;
+import dk.tbsalling.aismessages.ais.messages.AISMessage;
+import dk.tbsalling.aismessages.ais.messages.types.AISMessageType;
+import dk.tbsalling.aismessages.nmea.exceptions.InvalidMessage;
+import dk.tbsalling.aismessages.nmea.messages.NMEAMessage;
 
-public interface AbstractMessageData {
+import java.time.Instant;
 
-  public void checkIsSupported();
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static java.sql.Types.NULL;
 
-  public boolean getIsValidMsg();
+public class AbstractMessageData implements MessageDataInterface {
+    private AISMessage message;
+    private Instant timeRecieved;
+    private boolean isMultiPart;
+    private int geospatialDataId;
+    private int navigationDataId;
+    private int voyageDataId;
+    private int vesselSignatureId;
+    private int vesselDataId;
 
-  public int getMMSI();
+    public AbstractMessageData(AISMessage message) {
+        this.message = message;
 
-  public int getMsgTypeId();
+        this.timeRecieved = this.message.getMetadata().getReceived();
+        this.isMultiPart = FALSE;
+        this.geospatialDataId = NULL;
+        this.navigationDataId = NULL;
+        this.voyageDataId = NULL;
+        this.vesselSignatureId = NULL;
+        this.vesselDataId = NULL;
+    }
 
-  public String getRawNMEA();
+    public void processMessage() {
+        AISMessageType messageType = message.getMessageType();
 
+        if (messageType != null) {
+            switch (messageType) {
+
+                //Message Type 1
+                case PositionReportClassAScheduled:
+
+
+                    break;
+
+                //Message Type 2
+                case PositionReportClassAAssignedSchedule:
+
+
+                    break;
+
+                //Message Type 3
+                case PositionReportClassAResponseToInterrogation:
+
+
+                    break;
+
+                //Message Type 5
+                case ShipAndVoyageRelatedData:
+
+
+                    break;
+
+                //Unsupported Message
+                default:
+                    throw new UnsupportedMessageType(messageType.getCode());
+            }
+        } else {
+            //isInvalid Message field
+            throw new InvalidMessage("Invalid NMEA message");
+        }
+
+    }
+
+    public boolean isValidType() {
+        return this.getTypeId() > -1;
+    }
+
+    public int getMMSI() {
+        return this.message.getSourceMmsi().getMMSI();
+    }
+
+    public int getTypeId() {
+        return this.message.getMessageType().getCode();
+    }
+
+    public String getRawNMEA() {
+        NMEAMessage[] nmeaSentence = this.message.getNmeaMessages();
+        if (nmeaSentence.length > 1) {
+            this.isMultiPart = TRUE;
+            StringBuilder messages = new StringBuilder();
+
+            for (int i = 0; i < nmeaSentence.length; i++) {
+                messages.append(nmeaSentence[i].getRawMessage());
+
+                if (i != nmeaSentence.length - 1) {
+                    messages.append(" | ");
+                }
+            }
+            return messages.toString();
+        } else {
+            return nmeaSentence[0].getRawMessage();
+        }
+    }
 }
-
