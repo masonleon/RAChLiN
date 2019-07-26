0a72c87fb61cd5f5859c0e2dbbaaa766f5f601d1
Anand Kumar <kumara0398@gmail.com>
2019-07-26 16:16:24 -0400

    implementation details for wrapper class

--- aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/messageData/ClassAPositionReportData.java
+++ aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/messageData/ClassAPositionReportData.java
@@ -1,35 +1,64 @@
 package com.aisMessageListener.AisDecodeMessageStore.jdbc.messageData;
 
 import dk.tbsalling.aismessages.ais.messages.AISMessage;
+import dk.tbsalling.aismessages.ais.messages.PositionReport;
 import dk.tbsalling.aismessages.ais.messages.types.ManeuverIndicator;
 import dk.tbsalling.aismessages.ais.messages.types.NavigationStatus;
 
 public class ClassAPositionReportData extends AbstractMessageData {
 
-  private double lat;
-  private double lon;
-  private int accuracy;
-  private NavigationStatus navStatus;
-  private ManeuverIndicator maneuverIndicator;
-  private double speedOverGround;
-  private double courseOverGround;
-  private double heading;
-  private double rateOfTurn;
+    private final PositionReport positionReport;
 
-//  public ClassAPositionReport(ClassAPositionReport message) {
-//    super(message);
-//  }
+    public ClassAPositionReportData(AISMessage message) {
+        super(message);
 
-  public ClassAPositionReportData(AISMessage message) {
-    super(message);
-//    super(message);
-//  }
-  }
+        if (!(message instanceof PositionReport)) {
+            throw new IllegalArgumentException("Message is not a position report!");
+        }
 
-  public double getLat(){
+        this.positionReport = (PositionReport) message;
+    }
 
-//    if(message.getMessageType().equals())
-//    return this.message.getRate
-    return 0.0;
-  }
+    public ClassAPositionReportData(PositionReport positionReport) {
+        super(positionReport);
+
+        this.positionReport = positionReport;
+    }
+
+    public double getLat() {
+        return positionReport.getLatitude();
+    }
+
+    public double getLong() {
+        return positionReport.getLongitude();
+    }
+
+    public int getAccuracy() {
+        return positionReport.getPositionAccuracy() ? 1 : 0;
+    }
+
+    public NavigationStatus getNavStatus() {
+        return positionReport.getNavigationStatus();
+    }
+
+    public ManeuverIndicator getManeuverIndicator() {
+        return positionReport.getSpecialManeuverIndicator();
+    }
+
+    public double getSpeedOverGround() {
+        return positionReport.getSpeedOverGround();
+    }
+
+    public double getCourseOverGround() {
+        return positionReport.getCourseOverGround();
+    }
+
+    // TODO: internal implementations of both of these are integers?
+    public double getHeading() {
+        return positionReport.getTrueHeading();
+    }
+
+    public double getRateOfTurn() {
+        return positionReport.getRateOfTurn();
+    }
 }
