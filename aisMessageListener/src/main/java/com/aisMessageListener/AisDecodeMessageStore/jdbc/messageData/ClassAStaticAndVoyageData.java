4464d4e032da6648d8a15fc50799d629513d9bd7
Anand Kumar <kumara0398@gmail.com>
2019-07-26 16:47:38 -0400

    implementation for static voyage data wrapper

--- aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/messageData/ClassAStaticAndVoyageData.java
+++ aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/messageData/ClassAStaticAndVoyageData.java
@@ -1,12 +1,70 @@
 package com.aisMessageListener.AisDecodeMessageStore.jdbc.messageData;
 
-import com.aisMessageListener.AisDecodeMessageStore.jdbc.messageData.AbstractMessageData;
-
 import dk.tbsalling.aismessages.ais.messages.AISMessage;
+import dk.tbsalling.aismessages.ais.messages.ShipAndVoyageData;
+import dk.tbsalling.aismessages.ais.messages.types.IMO;
+import dk.tbsalling.aismessages.ais.messages.types.PositionFixingDevice;
+import dk.tbsalling.aismessages.ais.messages.types.ShipType;
 
 public class ClassAStaticAndVoyageData extends AbstractMessageData {
 
+  private final ShipAndVoyageData shipVoyageData;
+
   public ClassAStaticAndVoyageData(AISMessage message) {
     super(message);
+
+    if (!(message instanceof ShipAndVoyageData)) {
+      throw new IllegalArgumentException("Message is not a ship and voyage data!");
+    }
+
+    this.shipVoyageData = (ShipAndVoyageData) message;
+  }
+
+  public IMO getIMO() {
+    return shipVoyageData.getImo();
+  }
+
+  public String getCallsign() {
+    return shipVoyageData.getCallsign();
+  }
+
+  public String getShipName() {
+    return shipVoyageData.getShipName();
+  }
+
+  public ShipType getShipType() {
+    return shipVoyageData.getShipType();
+  }
+
+  public Integer getToBow() {
+    return shipVoyageData.getToBow();
+  }
+
+  public Integer getToStern() {
+    return shipVoyageData.getToStern();
+  }
+
+  public Integer getToStarboard() {
+    return shipVoyageData.getToStarboard();
+  }
+
+  public Integer getToPort() {
+    return shipVoyageData.getToPort();
+  }
+
+  public PositionFixingDevice getPositionFixingDevice() {
+    return shipVoyageData.getPositionFixingDevice();
+  }
+
+  public Float getDraught() {
+    return shipVoyageData.getDraught();
+  }
+
+  public String getDestination() {
+    return shipVoyageData.getDestination();
+  }
+
+  public Boolean isDataTerminalReady() {
+    return shipVoyageData.getDataTerminalReady();
   }
 }
