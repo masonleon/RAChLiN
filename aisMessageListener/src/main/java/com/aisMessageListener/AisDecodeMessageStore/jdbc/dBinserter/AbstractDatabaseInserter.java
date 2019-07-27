61d1ae18b2ff12e244b8261e03add15ef4eeb1da
Brian Rouse <brian.g.rouse@gmail.com>
2019-07-27 16:12:05 -0400

    Updated AisTcpClient to use writeMessage().

--- aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/dBinserter/AbstractDatabaseInserter.java
+++ aisMessageListener/src/main/java/com/aisMessageListener/AisDecodeMessageStore/jdbc/dBinserter/AbstractDatabaseInserter.java
@@ -3,179 +3,182 @@ package com.aisMessageListener.AisDecodeMessageStore.jdbc.dBinserter;
 
 import com.aisMessageListener.AisDecodeMessageStore.jdbc.DatabaseConnectionInterface;
 import com.aisMessageListener.AisDecodeMessageStore.jdbc.messageData.MessageDataInterface;
-import dk.tbsalling.aismessages.ais.exceptions.UnsupportedMessageType;
 
 import java.sql.SQLException;
 
+import dk.tbsalling.aismessages.ais.exceptions.UnsupportedMessageType;
+
 public abstract class AbstractDatabaseInserter implements DatabaseInserterInterface {
 
-    protected MessageDataInterface message;
-    protected DatabaseConnectionInterface connection;
+  protected MessageDataInterface message;
+  protected DatabaseConnectionInterface connection;
 
-    protected AbstractDatabaseInserter(MessageDataInterface message, DatabaseConnectionInterface connection) {
-        this(message);
+  protected AbstractDatabaseInserter(MessageDataInterface message, DatabaseConnectionInterface connection) {
+    this(message);
 
-        attachConnection(connection);
+    attachConnection(connection);
+  }
+
+  protected AbstractDatabaseInserter(MessageDataInterface message) {
+    this.message = message;
+  }
+
+  @Override
+  public void attachConnection(DatabaseConnectionInterface conn) {
+    this.connection = conn;
+  }
+
+  @Override
+  public WriteResult writeMessage() throws SQLException {
+    connection.connectIfDropped();
+    connection.beginTransaction();
+
+    try {
+      writeGeospatialData();
+      writeVoyageData();
+      writeNavigationStatus();
+      writeManeuverIndicator();
+      writeNavigationData();
+      writeVesselData();
+      writeVesselType();
+      writeVesselSignature();
+      writeMessageData();
+      connection.commitTransaction();
+      return WriteResult.SUCCESS;
+    } catch (SQLException ex) {
+      connection.rollBackTransaction();
     }
+    return WriteResult.FAILURE;
+  }
 
-    protected AbstractDatabaseInserter(MessageDataInterface message) {
-     this.message = message;
-   }
+  @Override
+  public WriteResult writeMessageData() {
+    return WriteResult.UNSUPPORTED;
+  }
 
-    @Override
-    public void attachConnection(DatabaseConnectionInterface conn) {
-       this.connection = conn;
+  @Override
+  public WriteResult writeVesselSignature() throws SQLException {
+    try {
+      message.getMMSI();
+      message.getIMO();
+      message.getCallsign();
+      message.getShipName();
+
+      // CHECK TO SEE IF THIS PERMUTATION IN TABLE FIRST THEN WRITE IF NECESSARY
+
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeMessage() throws SQLException {
-        connection.beginTransaction();
-        try {
-            writeGeospatialData();
-            writeVoyageData();
-            writeNavigationStatus();
-            writeManeuverIndicator();
-            writeNavigationData();
-            writeVesselData();
-            writeVesselType();
-            writeVesselSignature();
-            writeMessageData();
-            connection.commitTransaction();
-            return WriteResult.SUCCESS;
-        } catch (SQLException ex) {
-            connection.rollBackTransaction();
-        }
-        return WriteResult.FAILURE;
+  @Override
+  public WriteResult writeVoyageData() throws SQLException {
+    try {
+      message.getDraught();
+      message.getETA();
+      message.getDestination();
+
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeMessageData() {
-       return WriteResult.UNSUPPORTED;
+  @Override
+  public WriteResult writeVesselData() throws SQLException {
+    try {
+      message.getToBow();
+      message.getToPort();
+      message.getToStarboard();
+      message.getToStern();
+
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeVesselSignature() throws SQLException {
-        try {
-            message.getMMSI();
-            message.getIMO();
-            message.getCallsign();
-            message.getShipName();
+  @Override
+  public WriteResult writeVesselType() throws SQLException {
+    try {
 
-            // CHECK TO SEE IF THIS PERMUTATION IN TABLE FIRST THEN WRITE IF NECESSARY
+      message.getShipType();
 
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeVoyageData() throws SQLException {
-        try {
-            message.getDraught();
-            message.getETA();
-            message.getDestination();
+  @Override
+  public WriteResult writeNavigationStatus() throws SQLException {
+    try {
 
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeVesselData() throws SQLException {
-        try {
-            message.getToBow();
-            message.getToPort();
-            message.getToStarboard();
-            message.getToStern();
+  @Override
+  public WriteResult writeNavigationData() throws SQLException {
+    try {
+      message.getCourseOverGround();
+      message.getDestination();
+      message.getHeading();
+      message.getRateOfTurn();
 
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeVesselType() throws SQLException {
-        try {
+  @Override
+  public WriteResult writeGeospatialData() throws SQLException {
+    try {
+      message.getAccuracy();
+      // TODO: coord?
 
-            message.getShipType();
-
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
+  }
 
-    @Override
-    public WriteResult writeNavigationStatus() throws SQLException {
-        try {
+  @Override
+  public WriteResult writeManeuverIndicator() throws SQLException {
+    try {
+      message.getManeuverIndicator();
 
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
+      return WriteResult.SUCCESS;
+    } catch (UnsupportedMessageType ex) {
+      // WRITE BLANK RECORD WITH NULL COLUMNS
+      return WriteResult.UNSUPPORTED;
+    } catch (Exception ex) {
+      return WriteResult.FAILURE;
     }
-
-    @Override
-    public WriteResult writeNavigationData() throws SQLException {
-        try {
-            message.getCourseOverGround();
-            message.getDestination();
-            message.getHeading();
-            message.getRateOfTurn();
-
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
-    }
-
-    @Override
-    public WriteResult writeGeospatialData() throws SQLException {
-        try {
-            message.getAccuracy();
-            // TODO: coord?
-
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
-    }
-
-    @Override
-    public WriteResult writeManeuverIndicator() throws SQLException {
-        try {
-            message.getManeuverIndicator();
-
-            return WriteResult.SUCCESS;
-        } catch (UnsupportedMessageType ex) {
-            // WRITE BLANK RECORD WITH NULL COLUMNS
-            return WriteResult.UNSUPPORTED;
-        } catch (Exception ex) {
-            return WriteResult.FAILURE;
-        }
-    }
+  }
 }
